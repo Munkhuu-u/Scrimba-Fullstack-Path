2a6e9492e7a57e786b365719b4f4adc681f9bd3e
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2026-01-07 21:22:38 +0800

    modal
    modal

--- Essential JavaScript/Restaurant Ordering app/index.js
+++ Essential JavaScript/Restaurant Ordering app/index.js
@@ -5,26 +5,40 @@ document.addEventListener("click", function (e) {
     const foodId = e.target.dataset.add;
     pushToOrderArray(foodId);
     document.getElementById("order-section").style.display = "block";
+  } else if (e.target.id === "remove") {
+    removeOrder(e.target.dataset.orderid);
+  } else if (e.target.id === "completeOrderBtn") {
+    showPayModal();
   }
 });
+function showPayModal() {
+  console.log("button works");
+  document.getElementById("payModal").style.display = "block";
+  document.getElementById("modalOverlay").style.display = "block";
+}
+
+function removeOrder(orderID) {
+  const index = orderArray.findIndex((food) => food.orderID === orderID);
+  console.log(index);
+  orderArray.splice(index, 1);
+  renderOrderItems();
+}
 
 function pushToOrderArray(foodId) {
-  orderArray.push(
-    menuArray.filter((food) => {
-      return food.id == foodId;
-    })[0]
-  );
-  console.log(orderArray);
+  let originalObject = menuArray.filter((food) => food.id == foodId)[0];
+  let pushingObject = { ...originalObject, orderID: crypto.randomUUID() };
+  orderArray.push(pushingObject);
   renderOrderItems();
 }
 
 function renderOrderItems() {
   let orderString = "";
   orderArray.forEach((food) => {
-    orderString += `        <div class="order">
+    orderString += `
+        <div class="order">
           <div class="order-right">
             <p class="order-item">${food.name}</p>
-            <p class="order-remove">remove</p>
+            <p class="order-remove" data-orderid="${food.orderID}" id="remove">remove</p>
           </div>
           <p class="order-price">$${food.price}</p>
         </div>`;
