08ad561811585a22dc81d7f7101e11ce3426f80a
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2026-02-05 15:24:42 +0800

    Find your film page updates
    type-noos bolj hooshhi buh code ni ajilhaa bolidog ym baina, forEach bol huleedeggui method ym bn, even with async call back fn, ::before, custom border-radius for each peak,

--- Essential JavaScript/Movie Watchlist/index.js
+++ Essential JavaScript/Movie Watchlist/index.js
@@ -2,6 +2,7 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  e.target.movieName.blur();
   handleSubmit(e.target.movieName.value);
 });
 
@@ -12,39 +13,49 @@ async function handleSubmit(name) {
   getFullParameters(imdbIDs);
 }
 
-function getFullParameters(IDs) {
-  let movies = new Array();
-  IDs.forEach(async (id) => {
+async function getFullParameters(IDs) {
+  let movies = [];
+  for (const id of IDs) {
     const res = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=1e6b5110`);
     const data = await res.json();
     movies.push(data);
-  });
-  renderMovies(movies);
+  }
+
+  renderMovies(movies, IDs);
 }
 
-function renderMovies(movies) {
-  console.log("movies: ", movies);
-  let arr = Object.values(movies);
-  console.log("arr: ", arr);
-  console.log(typeof movies);
+function renderMovies(movies, IDs) {
   let context = "";
-  movies.forEach((movie) => {
-    console.log("forEach working...");
+
+  movies.forEach((movie, index) => {
     context += `<div class="movie">
-        <div>
-          <img src="${movie.Poster}" alt="poster-image">
-        </div>
-        <div class="textArea">
-          <p class="moiveTitle">${movie.Title}</p>
-          <div class="movieProps">
-            <p class="duration">${movie.RunTime}</p>
-            <p class="genres">${movie.Genre}</p>
-            <button class="addToWatchlist">+</button>
-          </div>
-          <p class="description">${movie.Description}</p>
-        </div>
-      </div>`;
+            <div class="movieImage" id="${movie.imdbID}"></div>
+            <div class="textArea">
+                <div class="movieTitleContainer">
+                    <p class="moiveTitle">${movie.Title}</p>
+                    <p>⭐</p>
+                    <p class="movieRating">${movie.Ratings[0].Value}</p>
+                </div>
+                <div class="movieProps">
+                    <p class="duration">${movie.Runtime}</p>
+                    <p class="genres">${movie.Genre}</p>
+                    <button class="addToWatchlist">+ Watchlist</button>
+                </div>
+              <p class="description">${movie.Plot}</p>
+            </div>
+          </div>`;
+    if (index + 1 !== movies.length) {
+      context += `<div class="seperator"></div>`;
+    }
   });
-  console.log("context: ", context);
+
+  document.getElementById("whenEmpty").style.display = "none";
+
   document.getElementById("movieContainer").innerHTML = context;
+
+  IDs.forEach((id, index) => {
+    document.getElementById(
+      `${id}`
+    ).style.backgroundImage = `url(${movies[index].Poster})`;
+  });
 }
